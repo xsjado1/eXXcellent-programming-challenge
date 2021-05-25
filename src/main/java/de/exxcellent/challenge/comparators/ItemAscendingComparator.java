9364d077b11bcd72f8ee9d8333dd3ddd311f0c32
xsjado1 <sjestaedt@googlemail.com>
2021-05-26 01:10:09 +0200

    add documentation

--- src/main/java/de/exxcellent/challenge/comparators/ItemAscendingComparator.java
+++ src/main/java/de/exxcellent/challenge/comparators/ItemAscendingComparator.java
@@ -1,14 +1,19 @@
 package de.exxcellent.challenge.comparators;
 
-import java.util.Comparator;
+/**
+ * This is a comparator to compare the absolute difference 
+ * of two items. 
+ * @author Sebastian Jestaedt
+ */
 
+import java.util.Comparator;
 import de.exxcellent.challenge.objects.Item;
 
 public class ItemAscendingComparator implements Comparator<Item>{
 
 	@Override
 	public int compare(Item item1, Item item2) {
-		return Integer.compare(item1.getTemperatureSpread(), item2.getTemperatureSpread());
+		return Integer.compare(item1.getAbsoluteDifference(), item2.getAbsoluteDifference());
 	}
 
 
--- src/main/java/de/exxcellent/challenge/comparators/ItemDescendingComparator.java
+++ src/main/java/de/exxcellent/challenge/comparators/ItemDescendingComparator.java
@@ -1,14 +1,19 @@
 package de.exxcellent.challenge.comparators;
 
-import java.util.Comparator;
+/**
+ * This is a comparator to compare the absolute difference 
+ * of two items. 
+ * @author Sebastian Jestaedt
+ */
 
+import java.util.Comparator;
 import de.exxcellent.challenge.objects.Item;
 
 public class ItemDescendingComparator implements Comparator<Item>{
 
 	@Override
 	public int compare(Item item1, Item item2) {
-		return Integer.compare(item2.getTemperatureSpread(), item1.getTemperatureSpread());
+		return Integer.compare(item2.getAbsoluteDifference(), item1.getAbsoluteDifference());
 	}
 
 
